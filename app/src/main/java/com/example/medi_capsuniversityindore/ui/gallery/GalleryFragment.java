b29d7291cd05f4ab4dfc136b6dcdd927fb0cde09
rahulpal80524 <rahulpal80524@gmail.com>
2021-10-19 13:41:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/medi_capsuniversityindore/ui/gallery/GalleryFragment.java
@@ -0,0 +1,22 @@
+package com.example.medi_capsuniversityindore.ui.gallery;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.medi_capsuniversityindore.R;
+
+public class GalleryFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_gallery, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/medi_capsuniversityindore/ui/home/HomeFragment.java
@@ -0,0 +1,24 @@
+package com.example.medi_capsuniversityindore.ui.home;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.medi_capsuniversityindore.R;
+
+
+public class HomeFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
